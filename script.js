8684ee79982a0fec00b7b0545e0e33f690a863d8
sarajalal2004 <sarauobeducation@gmail.com>
2025-09-14 12:54:09 +0300

    Complete main functionalities

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// Global variables
+// SARA JALAL ALI ALKHOZAAE
+// 11/9/2025 - 18/9/2025
+// Digitalize the famous Dots and Boxes game
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// Global variables /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////// playes variables - containing scores, turn, names - ////////////////////////////
 let turn = true // track the users turns
 let player1 = "Sara" // name of first player
 let player2 = "Ruqaya" // name of second player
@@ -6,34 +14,79 @@ let playerScore1 = 0
 let playerScore2 = 0
 const playerScore1Display = document.querySelector("#playerScore1")
 const playerScore2Display = document.querySelector("#playerScore2")
+
+//////////////////////////// game variables - containing dimensions and counts - ////////////////////////////
 const width = 7 // number of boxes columns
 const height = 4 // number of boxes rows
 let choiceCount = 0 // count the number of selected borders
 let addedCount = 0 // count index of border added to UI
-let isBorder = false // for adding to UI, know if border class needed
 const line = 2 * width + 1 // number of borders associate to line of boxes ignoring the bottom borders
 const numberOfGridDivs = (2 * width + 1) * (2 * height + 1) // the number of all grid divs
+
+////////////////////////////// DOM objects variables - accessing DOM objects - //////////////////////////////
 let borders // access the array of borders DOM
 let boxes // access the array of boxes DOM
 const gameBoard = document.querySelector("#gameBoard") // access the game board DOM object
 const turnMessage = document.querySelector("#turnMessage") // access the turn massage DOM object
-// functions
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////// functions /////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let startUI = () => {
+  let isBorder = false // for adding to UI, know if border class needed
   turnMessage.innerText = player1
-  for (let i = 0; i < numberOfGridDivs; i++) {
-    newDiv = document.createElement("div")
-    gameBoard.appendChild(newDiv)
-    if (addedCount % (2 * line) < line && isBorder) {
-      newDiv.classList.add("border", "horizontal")
-    } else if (addedCount % (2 * line) >= line && isBorder) {
-      newDiv.classList.add("border", "vertical")
-    } else if (addedCount % (2 * line) >= line && !isBorder) {
-      newDiv.classList.add("box")
-    } else {
-      newDiv.classList.add("dot")
+  for (let lineCount = 0; lineCount < 2 * height + 1; lineCount++) {
+    for (let itemCount = 0; itemCount < line; itemCount++) {
+      twoLinesCount = Math.floor(lineCount / 2)
+      boxCount = twoLinesCount * width + Math.floor(itemCount / 2)
+      newDiv = document.createElement("div")
+      gameBoard.appendChild(newDiv)
+
+      if (lineCount === 0) {
+        isBorder
+          ? newDiv.classList.add(`i${boxCount}0`, "border", "horizontal")
+          : newDiv.classList.add("dot")
+      } else if (lineCount > 0 && lineCount < 2 * height) {
+        if (lineCount % 2 == 0) {
+          isBorder
+            ? newDiv.classList.add(
+                `i${boxCount - width}2`,
+                `i${boxCount}0`,
+                "border",
+                "horizontal"
+              )
+            : newDiv.classList.add("dot")
+        } else {
+          if (isBorder) {
+            if (itemCount === 0) {
+              newDiv.classList.add(`i${boxCount}3`, "border", "vertical")
+            } else if (itemCount > 0 && itemCount < line - 1) {
+              newDiv.classList.add(
+                `i${boxCount - 1}1`,
+                `i${boxCount}3`,
+                "border",
+                "vertical"
+              )
+            } else {
+              newDiv.classList.add(`i${boxCount - 1}1`, "border", "vertical")
+            }
+          } else {
+            newDiv.classList.add(`i${boxCount}`, "box")
+          }
+        }
+      } else {
+        isBorder
+          ? newDiv.classList.add(
+              `i${boxCount - width}2`,
+              "border",
+              "horizontal"
+            )
+          : newDiv.classList.add("dot")
+      }
+
+      addedCount++
+      isBorder = !isBorder
     }
-    addedCount++
-    isBorder = !isBorder
   }
   gameBoard.style.gridTemplateColumns = `repeat(${width}, 15px ${
     100 / (1.25 * width)
@@ -58,85 +111,23 @@ let adjustWhenCompletedBox = (completedBoxIndex) => {
   choiceCount++
 }
 
-let checkWinHorizontal = (index) => {
+let checkWinBox = (index) => {
   isABoxCompleted = false
   let completedBoxIndex
-  if (index > width) {
-    //check up
-    console.log("up")
+  let classListCopy = [...borders[index].classList].slice(0, -3)
+  classListCopy.forEach((relationToBoxIndex) => {
+    box = parseInt(relationToBoxIndex.slice(1, -1))
     if (
-      borders[index - width].classList.contains("clicked") &&
-      borders[index - width - 1].classList.contains("clicked") &&
-      borders[index - 2 * width - 1].classList.contains("clicked")
+      document.querySelector(`.i${box}0`).classList.contains("clicked") &&
+      document.querySelector(`.i${box}1`).classList.contains("clicked") &&
+      document.querySelector(`.i${box}2`).classList.contains("clicked") &&
+      document.querySelector(`.i${box}3`).classList.contains("clicked")
     ) {
-      console.log("upSquare")
-      completedBoxIndex =
-        (index % line) + width * (Math.floor(index / line) - 1)
-      adjustWhenCompletedBox(completedBoxIndex)
+      adjustWhenCompletedBox(box)
       isABoxCompleted = true
     }
-  }
-  if (index < line * height) {
-    // check down
-    console.log("down")
-    if (
-      borders[index + width].classList.contains("clicked") &&
-      borders[index + width + 1].classList.contains("clicked") &&
-      borders[index + 2 * width + 1].classList.contains("clicked")
-    ) {
-      console.log("downSquare")
-      completedBoxIndex = (index % line) + width * Math.floor(index / line)
-      adjustWhenCompletedBox(completedBoxIndex)
-      isABoxCompleted = true
-    }
-  }
+  })
   return isABoxCompleted
-}
-
-let checkWinVertical = (index) => {
-  isABoxCompleted = false
-  if (index % line != width) {
-    //check left
-    console.log("left")
-    if (
-      borders[index - 1].classList.contains("clicked") &&
-      borders[index - width - 1].classList.contains("clicked") &&
-      borders[index + width].classList.contains("clicked")
-    ) {
-      console.log("leftSquare")
-      completedBoxIndex =
-        (index % line) - width - 1 + width * Math.floor(index / line)
-      console.log(completedBoxIndex)
-      adjustWhenCompletedBox(completedBoxIndex)
-      isABoxCompleted = true
-    }
-  }
-  if (index % line != line - 1) {
-    // check right
-    console.log("right")
-    if (
-      borders[index + 1].classList.contains("clicked") &&
-      borders[index + width + 1].classList.contains("clicked") &&
-      borders[index - width].classList.contains("clicked")
-    ) {
-      console.log("rightSquare")
-      completedBoxIndex =
-        (index % line) - width + width * Math.floor(index / line)
-      adjustWhenCompletedBox(completedBoxIndex)
-      isABoxCompleted = true
-    }
-  }
-  return isABoxCompleted
-}
-
-let checkWinBox = (index) => {
-  console.log(index)
-  if (borders[index].classList.contains("horizontal")) {
-    if (checkWinHorizontal(index)) return true
-  } else if (borders[index].classList.contains("vertical")) {
-    if (checkWinVertical(index)) return true
-  }
-  return false
 }
 
 //logic
---
+++ scriptEquations.js
@@ -0,0 +1,162 @@
+// Global variables
+let turn = true // track the users turns
+let player1 = "Sara" // name of first player
+let player2 = "Ruqaya" // name of second player
+let playerScore1 = 0
+let playerScore2 = 0
+const playerScore1Display = document.querySelector("#playerScore1")
+const playerScore2Display = document.querySelector("#playerScore2")
+const width = 7 // number of boxes columns
+const height = 4 // number of boxes rows
+let choiceCount = 0 // count the number of selected borders
+let addedCount = 0 // count index of border added to UI
+let isBorder = false // for adding to UI, know if border class needed
+const line = 2 * width + 1 // number of borders associate to line of boxes ignoring the bottom borders
+const numberOfGridDivs = (2 * width + 1) * (2 * height + 1) // the number of all grid divs
+let borders // access the array of borders DOM
+let boxes // access the array of boxes DOM
+const gameBoard = document.querySelector("#gameBoard") // access the game board DOM object
+const turnMessage = document.querySelector("#turnMessage") // access the turn massage DOM object
+// functions
+let startUI = () => {
+  turnMessage.innerText = player1
+  for (let i = 0; i < numberOfGridDivs; i++) {
+    newDiv = document.createElement("div")
+    gameBoard.appendChild(newDiv)
+    if (addedCount % (2 * line) < line && isBorder) {
+      newDiv.classList.add("border", "horizontal")
+    } else if (addedCount % (2 * line) >= line && isBorder) {
+      newDiv.classList.add("border", "vertical")
+    } else if (addedCount % (2 * line) >= line && !isBorder) {
+      newDiv.classList.add("box")
+    } else {
+      newDiv.classList.add("dot")
+    }
+    addedCount++
+    isBorder = !isBorder
+  }
+  gameBoard.style.gridTemplateColumns = `repeat(${width}, 15px ${
+    100 / (1.25 * width)
+  }vh) 15px`
+  gameBoard.style.gridTemplateRows = `repeat(${height}, 15px ${
+    100 / (1.25 * width)
+  }vh) 15px`
+  borders = document.querySelectorAll(".border")
+  boxes = document.querySelectorAll(".box")
+}
+
+let adjustWhenCompletedBox = (completedBoxIndex) => {
+  if (turn) {
+    boxes[completedBoxIndex].style.backgroundColor = "lightcoral"
+    playerScore1++
+    playerScore1Display.innerText = playerScore1
+  } else {
+    boxes[completedBoxIndex].style.backgroundColor = "blue"
+    playerScore2++
+    playerScore2Display.innerText = playerScore2
+  }
+  choiceCount++
+}
+
+let checkWinHorizontal = (index) => {
+  isABoxCompleted = false
+  let completedBoxIndex
+  if (index > width) {
+    //check up
+    console.log("up")
+    if (
+      borders[index - width].classList.contains("clicked") &&
+      borders[index - width - 1].classList.contains("clicked") &&
+      borders[index - 2 * width - 1].classList.contains("clicked")
+    ) {
+      console.log("upSquare")
+      completedBoxIndex =
+        (index % line) + width * (Math.floor(index / line) - 1)
+      adjustWhenCompletedBox(completedBoxIndex)
+      isABoxCompleted = true
+    }
+  }
+  if (index < line * height) {
+    // check down
+    console.log("down")
+    if (
+      borders[index + width].classList.contains("clicked") &&
+      borders[index + width + 1].classList.contains("clicked") &&
+      borders[index + 2 * width + 1].classList.contains("clicked")
+    ) {
+      console.log("downSquare")
+      completedBoxIndex = (index % line) + width * Math.floor(index / line)
+      adjustWhenCompletedBox(completedBoxIndex)
+      isABoxCompleted = true
+    }
+  }
+  return isABoxCompleted
+}
+
+let checkWinVertical = (index) => {
+  isABoxCompleted = false
+  if (index % line != width) {
+    //check left
+    console.log("left")
+    if (
+      borders[index - 1].classList.contains("clicked") &&
+      borders[index - width - 1].classList.contains("clicked") &&
+      borders[index + width].classList.contains("clicked")
+    ) {
+      console.log("leftSquare")
+      completedBoxIndex =
+        (index % line) - width - 1 + width * Math.floor(index / line)
+      console.log(completedBoxIndex)
+      adjustWhenCompletedBox(completedBoxIndex)
+      isABoxCompleted = true
+    }
+  }
+  if (index % line != line - 1) {
+    // check right
+    console.log("right")
+    if (
+      borders[index + 1].classList.contains("clicked") &&
+      borders[index + width + 1].classList.contains("clicked") &&
+      borders[index - width].classList.contains("clicked")
+    ) {
+      console.log("rightSquare")
+      completedBoxIndex =
+        (index % line) - width + width * Math.floor(index / line)
+      adjustWhenCompletedBox(completedBoxIndex)
+      isABoxCompleted = true
+    }
+  }
+  return isABoxCompleted
+}
+
+let checkWinBox = (index) => {
+  console.log(index)
+  if (borders[index].classList.contains("horizontal")) {
+    if (checkWinHorizontal(index)) return true
+  } else if (borders[index].classList.contains("vertical")) {
+    if (checkWinVertical(index)) return true
+  }
+  return false
+}
+
+//logic
+startUI()
+
+// event listener
+borders.forEach((border, index) => {
+  border.addEventListener("click", () => {
+    if (!border.classList.contains("clicked")) {
+      border.classList.add("clicked")
+      if (checkWinBox(index)) {
+        if (choiceCount >= width * height) {
+          // complete
+        }
+      } else {
+        turn = !turn
+        turn
+          ? (turnMessage.innerText = player1)
+          : (turnMessage.innerText = player2)
+      }
+    }
+  })
+})
